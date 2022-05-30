d40c560debd80fe690f3152e2e798c576f1c9c3e
uwish <pin-999@yandex.ru>
2022-05-30 20:30:34 +0500

    deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/frontend-challenge' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/frontend-challenge' : '/',
   transpileDependencies: true
 })
